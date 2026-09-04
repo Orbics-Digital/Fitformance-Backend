c0c63dce355f89cdb13814d12df4c4fef5aecdda
alishanorbics <alishan.n@orbics.co>
2026-09-04 17:12:58 +0500

    Implement deleteExercise functionality with file removal and update related models. Enhance folder helper with logging.

--- src/helpers/folder.js
+++ src/helpers/folder.js
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import logger from '../config/logger.js'
 
 export const makeFolders = () => {
 
--- src/routes/exercise.route.js
+++ src/routes/exercise.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addExercise, getExerciseById, getExercises } from '../controllers/exercise.controller.js'
+import { addExercise, deleteExercise, getExerciseById, getExercises } from '../controllers/exercise.controller.js'
 import { CREATE_REHAB_VALIDATOR } from '../helpers/validators.js'
 import { AuthVerifier } from '../middleware/auth.middleware.js'
 import upload from '../middleware/upload.middleware.js'
@@ -12,5 +12,7 @@ router.get('/get', AuthVerifier, getExercises)
 router.get('/get/:id', AuthVerifier, getExerciseById)
 
 router.post('/create', AuthVerifier, upload('rehab').single('file'), validator(CREATE_REHAB_VALIDATOR), addExercise)
+
+router.delete('/delete/:id', AuthVerifier, deleteExercise)
 
 export default router
